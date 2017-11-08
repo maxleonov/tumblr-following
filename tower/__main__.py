97ff24f08aede03858ca38bcca4d167c8ddd0c38
Max Leonov <maks.leonov@gmail.com>
2017-11-08 10:10:02 +0100

    Fixes #5 suggest-unfollowing

--- tower/__main__.py
+++ tower/__main__.py
@@ -4,6 +4,7 @@ from tower.commands.fetch_following import fetch_following
 from tower.commands.fetch_posts import fetch_posts
 from tower.commands.top_sources import top_sources
 from tower.commands.top_following import reblogs_per_source
+from tower.commands.suggest_unfollowing import suggest_unfollowing
 from tower.database import engine
 from tower.log import setup_logging
 from tower.model import Base
@@ -19,6 +20,7 @@ main.add_command(fetch_posts)
 main.add_command(fetch_following)
 main.add_command(top_sources)
 main.add_command(reblogs_per_source)
+main.add_command(suggest_unfollowing)
 
 if __name__ == '__main__':
     main()
---
+++ tower/commands/suggest_unfollowing.py
@@ -0,0 +1,48 @@
+from datetime import datetime, timedelta
+import dateutil
+
+import click
+import pandas as pd
+from sqlalchemy import func, desc, and_
+
+from tower.database import Session
+from tower.model import Post, Following
+
+
+@click.command('suggest-unfollowing')
+@click.argument('user-name')
+@click.argument('blog-name')
+@click.option('--top', default=100)
+@click.option('--since-date', default=(datetime.today()-timedelta(days=3)).strftime('%Y-%m-%d'))
+def suggest_unfollowing(user_name: str, blog_name: str, top: int, since_date: str):
+    since_date = dateutil.parser.parse(since_date)
+
+    session = Session()
+
+    df = pd.read_sql(
+        session.query(Following.blog_name, func.count(Post.id).label('posts'))
+        .filter(
+            Following.user_name == user_name
+        )
+        .outerjoin(Post, and_(
+            (Following.blog_name == Post.reblogged_from_name) | (Following.blog_name == Post.reblogged_root_name),
+            Post.date >= since_date, Post.blog_name == blog_name)
+        )
+        .group_by(Following.blog_name).order_by(desc('posts')).limit(top).statement,
+        session.bind
+    )
+
+    print(  # TODO improve wording
+        'Some blogs are being followed but have zero reblogs to {} since {}.\n'
+        'The idea here is that a user might want to unfollow blogs that they follow don\'t reblog.\n'
+        'Please use at your own risk and make sure you udnerstand what the below code does.\n'
+        'Consider unfollowing them:\n'
+        'python tower/interactive_console.py\n'
+        'blogs = {}\n'
+        'for blog in blogs:\n'
+        '    client.unfollow(blog)'
+        .format(
+            blog_name, since_date.strftime('%Y-%m-%d'),
+            [blog for blog in list(df.blog_name[df.posts == 0])]
+        )
+    )
